ea71fe167bbe96a48d8fdef7be2f3adbcb542eeb
Sebastian Bensusan <sbensu@gmail.com>
2015-05-23 11:35:49 +0200

    Basic elements in place for example

--- src/example/core.cljs
+++ src/example/core.cljs
@@ -1,27 +1,63 @@
 (ns ^:figwheel-always example.core
-    (:require[om.core :as om :include-macros true]
-              [om.dom :as dom :include-macros true]))
+    (:require-macros [cljs.core.async.macros :refer [go go-loop]])
+    (:require [cljs.core.async :as async :refer (<! >! put! chan)]
+              [om.core :as om :include-macros true]
+              [om-tools.dom :as dom]
+              [om-tools.core :refer-macros [defcomponent]]
+              [s3-beam.client :refer [s3-pipe]]))
 
 (enable-console-print!)
 
-(println "Edits to this text should show up in your developer console.")
+(defn handle-drop
+  "Extracts the files out of the event e and puts them in the channel"
+  [ch e]
+  (.preventDefault e)
+  (.stopPropagation e)
+  (let [fs (or (.. e -target -files) 
+               (.. e -target -value)
+               (.. e -dataTransfer -files))]
+    (put! ch (aget fs 0))))
 
-;; define your app data so that it doesn't get over-written on reload
+(defcomponent file-input [data owner {:keys [ch]}]
+  (render [_]
+    (dom/input {:type "file"
+                :on-change (partial handle-drop ch)})))
 
-(defonce app-state (atom {:text "Hello world!"}))
+(defcomponent drop-area [data owner {:keys [ch]}]
+  (render-state [_ {:keys [hoover?]}]
+    (dom/div {:class (str "drop-area " (when hoover? "dragging"))
+              :on-drag-over (fn [e]
+                              (.preventDefault e)
+                              (set! (.. e -dataTransfer -dropEffect) "copy")
+                              (when-not hoover?
+                                (om/set-state! owner :hoover? true)))
+              :on-drag-leave (fn [_]
+                               (when hoover?
+                                 (om/set-state! owner :hoover? false)))
+              :on-drop (fn [e]
+                         (om/set-state! owner :hoover? false)
+                         (handle-drop ch e))}
+      (om/build file-input {} {:opts {:ch ch}}))))
 
-(om/root
-  (fn [data owner]
-    (reify om/IRender
-      (render [_]
-        (dom/h1 nil (:text data)))))
-  app-state
-  {:target (. js/document (getElementById "app"))})
+(defonce app-state (atom {:text "File Uploader"}))
 
+(defcomponent main [data owner]
+  (init-state [_]
+    (let [uploaded (chan)]
+      {:dropped-queue (chan)
+       :upload-queue (s3-pipe uploaded) 
+       :uploaded uploaded
+       :hoover? false}))
+  (will-mount [_]
+    (go-loop []
+      (let [fs (<! (om/get-state owner :uploaded))]
+        (.log js/console fs))
+      (recur)))
+  (render-state [_ {:keys [upload-queue]}]
+    (dom/div
+      (dom/h1 (:text data))
+      (om/build drop-area {} {:opts {:ch upload-queue}}))))
 
-(defn on-js-reload []
-  ;; optionally touch your app-state to force rerendering depending on
-  ;; your application
-  ;; (swap! app-state update-in [:__figwheel_counter] inc)
-) 
+(om/root main app-state {:target (. js/document (getElementById "app"))})
 
+(defn on-js-reload [] nil) 
